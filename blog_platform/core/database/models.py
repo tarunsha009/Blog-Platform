d9e93788961bd271c4825ba5acdfff0e6b2717b9
Tarun <tarunsha009@gmail.com>
2024-07-01 14:43:00 +0530

    Modification

---
+++ blog_platform/core/database/models.py
@@ -0,0 +1,15 @@
+from blog_platform.core.database.db import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
